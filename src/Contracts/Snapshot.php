b6f65d5bd55efe51b46c6f4079143024b07cf873
Michael Jennings <michael.jennings91@gmail.com>
2015-05-03 17:18:35 +0100

    Updated the snapshot contract

--- src/Contracts/Snapshot.php
+++ src/Contracts/Snapshot.php
@@ -2,6 +2,25 @@
 
 interface Snapshot {
 
+    /**
+     * Capture the current state of the application.
+     */
+    public function capture();
 
+    /**
+     * Find a snapshot by its id.
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function find($id);
+
+    /**
+     * Find a snapshot by its id and render all of it's data.
+     *
+     * @param $id
+     * @return string
+     */
+    public function render($id);
 
 }
