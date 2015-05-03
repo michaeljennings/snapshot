590df2881563fbb41d0256d3974c174b3a2cda76
Michael Jennings <michael.jennings91@gmail.com>
2015-05-03 18:20:52 +0100

    Added methods to get snapshot data

--- src/Store/Eloquent/Snapshot.php
+++ src/Store/Eloquent/Snapshot.php
@@ -49,4 +49,74 @@ class Snapshot extends Model {
         return $this->items;
     }
 
+    /**
+     * Get all of the server variables at the time of the snapshot.
+     *
+     * @return array|mixed
+     */
+    public function getServer()
+    {
+        return $this->server ? json_decode($this->server) : [];
+    }
+
+    /**
+     * Get all post data from the snapshot.
+     *
+     * @return array|mixed
+     */
+    public function getPost()
+    {
+        return $this->post ? json_decode($this->post) : [];
+    }
+
+    /**
+     * Get all get data from the snapshot.
+     *
+     * @return array|mixed
+     */
+    public function getGet()
+    {
+        return $this->get ? json_decode($this->get) : [];
+    }
+
+    /**
+     * Get all file data from the snapshot.
+     *
+     * @return array|mixed
+     */
+    public function getFiles()
+    {
+        return $this->files ? json_decode($this->files) : [];
+    }
+
+    /**
+     * Get the cookie data from the snapshot.
+     *
+     * @return array|mixed
+     */
+    public function getCookies()
+    {
+        return $this->cookies ? json_decode($this->cookies) : [];
+    }
+
+    /**
+     * Get the session data from the snapshot.
+     *
+     * @return array|mixed
+     */
+    public function getSession()
+    {
+        return $this->session ? json_decode($this->session) : [];
+    }
+
+    /**
+     * Get the environment data from the snapshot.
+     *
+     * @return array|mixed
+     */
+    public function getEnvironment()
+    {
+        return $this->environment ? json_decode($this->environment) : [];
+    }
+
 }
