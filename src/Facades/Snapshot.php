5950343ab35d51d2a5a6efa2603faed6d0e1e1d0
Michael Jennings <michael.jennings91@gmail.com>
2015-05-03 18:37:32 +0100

    Added laravel 5 facade

---
+++ src/Facades/Snapshot.php
@@ -0,0 +1,12 @@
+<?php namespace Michaeljennings\Snapshot\Facades; 
+
+class Snapshot {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'michaeljennings.snapshot'; }
+
+}
