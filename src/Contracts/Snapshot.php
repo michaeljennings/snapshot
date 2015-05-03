4082126d2cf6aae591a0795b93704c2ec0fda719
Michael Jennings <michael.jennings91@gmail.com>
2015-05-03 15:41:52 +0100

    Added snapshot and store contracts

---
+++ src/Contracts/Snapshot.php
@@ -0,0 +1,7 @@
+<?php namespace Michaeljennings\Snapshot\Contracts;
+
+interface Snapshot {
+
+
+
+}
---
+++ src/Contracts/Store.php
@@ -0,0 +1,13 @@
+<?php namespace Michaeljennings\Snapshot\Contracts;
+
+interface Store {
+
+    /**
+     * Store the current state of the application.
+     *
+     * @param array $input
+     * @return mixed
+     */
+    public function capture(array $input);
+
+}
