5c57d6e5335ce67796b4a4de32cb0072e4cd1544
Michael Jennings <michael.jennings91@gmail.com>
2015-05-03 15:41:10 +0100

    Added the laravel 5 service provider

---
+++ src/SnapshotServiceProvider.php
@@ -0,0 +1,51 @@
+<?php namespace Michaeljennings\Snapshot; 
+
+use Illuminate\Support\ServiceProvider;
+
+class SnapshotServiceProvider extends ServiceProvider {
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([__DIR__.'/../config/snapshot.php' => config_path('snapshot.php')]);
+        $this->mergeConfigFrom(__DIR__.'/../config/snapshot.php', 'snapshot');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerStore();
+
+        $this->app->singleton('Michaeljennings\Snapshot\Contracts\Snapshot', function($app)
+        {
+            return new Snapshot($app['Michaeljennings\Snapshot\Contracts\Store']);
+        });
+
+        $this->app->bind('michaeljennings.snapshot', function($app)
+        {
+            return $app['Michaeljennings\Snapshot\Contracts\Snapshot'];
+        });
+    }
+
+    /**
+     * Register the snapshot store.
+     */
+    protected function registerStore()
+    {
+        $store = config('snapshot.store');
+
+        $this->app->bind('Michaeljennings\Snapshot\Contracts\Store', function() use ($store)
+        {
+            return new $store;
+        });
+    }
+
+}
