d2f8641bc6a01af01742cd7e154777b503d44864
Michael Jennings <michael.jennings91@gmail.com>
2015-05-03 23:35:16 +0100

    Added comments to package config

--- config/snapshot.php
+++ config/snapshot.php
@@ -2,10 +2,38 @@
 
 return [
 
+    /**
+     * -------------------------------------------------------------------------
+     *  Store
+     * -------------------------------------------------------------------------
+     *
+     * Set the class to use to store the snapshots.
+     *
+     * Supported: Michaeljennings\\Snapshot\\Store\\Eloquent\\Store
+     *
+     */
     'store' => 'Michaeljennings\\Snapshot\\Store\\Eloquent\\Store',
 
+    /**
+     * -------------------------------------------------------------------------
+     *  Renderer
+     * -------------------------------------------------------------------------
+     *
+     * Set the class to use to render the snapshots.
+     *
+     * Supported: Michaeljennings\\Snapshot\\Renderers\\Illuminate
+     *            Michaeljennings\\Snapshot\\Renderers\\Native
+     *
+     */
     'renderer' => 'Michaeljennings\\Snapshot\\Renderers\\Illuminate',
 
+    /**
+     * -------------------------------------------------------------------------
+     *  View
+     * -------------------------------------------------------------------------
+     *
+     * Set the view to use to render the snapshots.
+     */
     'view' => 'snapshot::bootstrap.snapshot'
 
 ];
