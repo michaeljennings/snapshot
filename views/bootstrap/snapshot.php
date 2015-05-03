d82dfe8de0b8f48f415129fd82a1ab2a6c361abf
Michael Jennings <michael.jennings91@gmail.com>
2015-05-03 17:58:21 +0100

    Started creating the bootstrap view

--- views/bootstrap/snapshot.php
+++ views/bootstrap/snapshot.php
@@ -1,1 +1,18 @@
-<p>test</p>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<div class="col-sm-4">
+    <ul class="list-group">
+        <?php foreach ($snapshot->getItems() as $item): ?>
+            <li class="list-group-item">
+                <h4 class="list-group-item-heading"><?= $item['class']; ?> <?= $item['function'] ?></h4>
+                <?php if ( ! empty($item['file'])): ?>
+                    <p><?= $item['file'] ?>:<?= $item['line'] ?></p>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<div class="col-sm-8">
+    <?php foreach ($snapshot->getItems() as $item): ?>
+
+    <?php endforeach; ?>
+</div>
