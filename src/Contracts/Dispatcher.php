d3536765f5324ede4cb3885ad6cff7c4cd95f127
Michael Jennings <michael.jennings91@gmail.com>
2016-01-14 17:49:09 +0000

    Add methods to dispatcher contract

--- src/Contracts/Dispatcher.php
+++ src/Contracts/Dispatcher.php
@@ -4,5 +4,21 @@ namespace Michaeljennings\Snapshot\Contracts;
 
 interface Dispatcher
 {
+    /**
+     * Fire an event.
+     *
+     * @param mixed $event
+     * @param array $data
+     * @return \League\Event\EventInterface
+     */
+    public function emit($event, array $data);
 
+    /**
+     * Add an event listener.
+     *
+     * @param mixed $event
+     * @param mixed $listener
+     * @return \Michaeljennings\Snapshot\Dispatcher
+     */
+    public function listen($event, $listener);
 }
