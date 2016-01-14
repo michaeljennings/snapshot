eeb203250f343d85791b895fecc6394e63ac44f4
Michael Jennings <michael.jennings91@gmail.com>
2016-01-14 17:46:09 +0000

    Add event dispatcher

---
+++ src/Contracts/Dispatcher.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Michaeljennings\Snapshot\Contracts;
+
+interface Dispatcher
+{
+
+}
---
+++ src/Dispatcher.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Michaeljennings\Snapshot;
+
+use League\Event\EmitterInterface;
+use Michaeljennings\Snapshot\Contracts\Dispatcher as DispatcherContract;
+
+class Dispatcher implements DispatcherContract
+{
+    /**
+     * The event emitter implementation.
+     *
+     * @var EmitterInterface
+     */
+    protected $emitter;
+
+    /**
+     * Dispatcher constructor.
+     *
+     * @param EmitterInterface $emitter
+     */
+    public function __construct(EmitterInterface $emitter)
+    {
+        $this->emitter = $emitter;
+    }
+
+    /**
+     * Fire an event.
+     *
+     * @param mixed $event
+     * @param array $data
+     * @return \League\Event\EventInterface
+     */
+    public function emit($event, array $data)
+    {
+        return $this->emitter->emit($event, $data);
+    }
+
+    /**
+     * Add an event listener.
+     *
+     * @param mixed $event
+     * @param mixed $listener
+     * @return $this
+     */
+    public function listen($event, $listener)
+    {
+        return $this->emitter->addListener($event, $listener);
+    }
+}
