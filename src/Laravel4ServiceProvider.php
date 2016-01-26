38a0d6d547a359feb093ad466aaac45eff724a93
Michael Jennings <michael.jennings91@gmail.com>
2016-01-26 17:49:08 +0000

    Fix laravel 4 service provider config issues

--- src/Laravel4ServiceProvider.php
+++ src/Laravel4ServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace Michaeljennings\Snapshot;
 
+use League\Event\Emitter;
+use ReflectionClass;
+
 class Laravel4ServiceProvider extends SnapshotServiceProvider
 {
     /**
@@ -13,14 +16,57 @@ class Laravel4ServiceProvider extends SnapshotServiceProvider
     }
 
     /**
-     * Get the package config.
+     * Register the service provider.
      *
-     * @return array
+     * @return void
      */
-    protected function getConfig()
+    public function register()
     {
-        \Config::addNamespace('snapshot', realpath(__DIR__ . '/../config'));
+        $this->registerStore();
+        $this->registerRenderer();
+        $this->registerDispatcher();
 
-        return $this->app['config']['snapshot::snapshot'];
+        $this->app->singleton('Michaeljennings\Snapshot\Contracts\Snapshot', function ($app) {
+            return new Snapshot(
+                $app['Michaeljennings\Snapshot\Contracts\Store'],
+                $app['Michaeljennings\Snapshot\Contracts\Renderer'],
+                $app['Michaeljennings\Snapshot\Contracts\Dispatcher'],
+                $app['config']['snapshot::snapshot']
+            );
+        });
+
+        $this->app->alias('Michaeljennings\Snapshot\Contracts\Snapshot', 'snapshot');
+    }
+
+    /**
+     * Register the snapshot store.
+     */
+    protected function registerStore()
+    {
+        $this->app->bind('Michaeljennings\Snapshot\Contracts\Store', function ($app) {
+            return (new ReflectionClass($app['config']['snapshot::snapshot']['store']['class']))
+                ->newInstanceArgs([$app['config']['snapshot::snapshot']]);
+        });
+    }
+
+    /**
+     * Register the snapshot renderer.
+     */
+    protected function registerRenderer()
+    {
+        $this->app->bind('Michaeljennings\Snapshot\Contracts\Renderer', function ($app) {
+            return (new ReflectionClass($app['config']['snapshot::snapshot']['renderer']))
+                ->newInstanceArgs([$app['view']]);
+        });
+    }
+
+    /**
+     * Register the snapshot event dispatcher.
+     */
+    protected function registerDispatcher()
+    {
+        $this->app->singleton('Michaeljennings\Snapshot\Contracts\Dispatcher', function() {
+            return new Dispatcher(new Emitter());
+        });
     }
 }
